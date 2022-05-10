eab0576f63ce1971da2364cfe6527e97dbb5ba00
Carlos Alberto <caalb.craft@gmail.com>
2022-05-10 19:37:50 -0300

    nlw DoctorCare

---
+++ main.js
@@ -0,0 +1,72 @@
+onScroll()
+window.addEventListener('scroll', onScroll)
+
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+    scrollY !== 0 ? navigation.classList.add('scroll') : navigation.classList.remove('scroll')
+}
+
+function showBackToTopButtonOnScroll() {
+    scrollY > 1400 ? backToTopButton.classList.add('show') : backToTopButton.classList.remove('show')
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+function backToTop() {
+    scrollY >= 30 ? backToTopButton.classList.add('show') : none
+}
+
+
+ScrollReveal({
+    origin:'top',
+    distance: '30px',
+    duration: 1000,
+}).reveal(`#home, 
+#home img, 
+#home .stats, 
+#services,
+#services main,
+#services .card,
+#about,
+#about header,
+#about .content`);
